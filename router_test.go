e76dfa45356a5ce0719a969624a55fcbbc9fad8b
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2016-11-23 12:50:49 +0100

    Added test cases for Routes

--- router_test.go
+++ router_test.go
@@ -2,44 +2,75 @@ package mux
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-type PathTest struct {
-	title string
-	path  string
+type routeTest struct {
+	title      string
+	path       string
+	method     string
+	statusCode int
 }
 
-func TestHost(t *testing.T) {
+func TestPath(t *testing.T) {
 
-	tests := []PathTest{
+	tests := []routeTest{
 		{
-			title: "Path route with single path , match",
-			path:  "/api/",
+			title:      "Path route with single path",
+			path:       "/api/",
+			method:     http.MethodGet,
+			statusCode: http.StatusOK,
+		},
+		{
+			title:      "Path route with single path",
+			path:       "/api/users/",
+			method:     http.MethodGet,
+			statusCode: http.StatusOK,
+		},
+		{
+			title:      "Path route with single path",
+			path:       "/api/echo",
+			method:     http.MethodPost,
+			statusCode: http.StatusOK,
+		},
+		{
+			title:      "Path route with single path",
+			path:       "/api/echo",
+			method:     http.MethodDelete,
+			statusCode: http.StatusOK,
 		},
 	}
 
 	for _, test := range tests {
-		code, ok := testGET(test)
-
-		if !ok {
-			t.Errorf("Expected status code 200, Actucal status code %v", code)
-		}
+		t.Run(fmt.Sprintf("Test: %s path: %s method %s", test.title, test.path, test.method), func(t *testing.T) {
+			code, ok := testRoute(test)
+			if !ok {
+				t.Errorf("Expected status code %v, Actucal status code %v", test.statusCode, code)
+			}
+		})
 	}
 }
 
-func testGET(pt PathTest) (int, bool) {
+func testRoute(rt routeTest) (int, bool) {
 	handler := func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
+		w.Write([]byte("succesfully"))
 	}
 	r := NewRouter()
 
-	r.Get(pt.path, handler)
+	switch rt.method {
+	case http.MethodGet:
+		r.Get(rt.path, handler)
+	case http.MethodPost:
+		r.Post(rt.path, handler)
+	case http.MethodDelete:
+		r.Delete(rt.path, handler)
+	}
 
-	req, _ := http.NewRequest("GET", "http://localhost"+pt.path, nil)
+	req, _ := http.NewRequest(rt.method, "http://localhost"+rt.path, nil)
 	res := httptest.NewRecorder()
 	r.ServeHTTP(res, req)
 
@@ -50,7 +81,7 @@ func testGET(pt PathTest) (int, bool) {
 		return -1, false
 	}
 
-	if res.Code != 200 && content.String() == "Hello World!" {
+	if res.Code != rt.statusCode && content.String() == "succesfully" {
 		return res.Code, false
 	}
 
