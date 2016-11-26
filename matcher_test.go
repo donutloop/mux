85f283eb1f15d44d1340f7e99e5e59c3a08b4f71
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2016-11-26 13:18:55 +0100

    Added new test cases for matcher

--- matcher_test.go
+++ matcher_test.go
@@ -20,3 +20,68 @@ func TestSchemeMatcher(t *testing.T) {
 		}
 	}
 }
+
+func TestSchemeMatcherFail(t *testing.T) {
+	schemes := []string{"http", "https"}
+	matcher := newSchemeMatcher("httpss")
+	request := &http.Request{
+		URL: &url.URL{},
+	}
+
+	for _, v := range schemes {
+		request.URL.Scheme = v
+		if matcher.Match(request) {
+			t.Errorf("Scheme matched (%v)", v)
+		}
+	}
+}
+
+func TestPathMatcher(t *testing.T) {
+	matcher := pathMatcher("/api/v2")
+	request := &http.Request{
+		URL: &url.URL{
+			Path: "/api/v2",
+		},
+	}
+
+	if !matcher.Match(request) {
+		t.Errorf("Unexpected not matched path ")
+	}
+}
+
+func TestPathMatcherFail(t *testing.T) {
+	matcher := pathMatcher("/api/v2")
+	request := &http.Request{
+		URL: &url.URL{
+			Path: "/api/v1",
+		},
+	}
+
+	if matcher.Match(request) {
+		t.Errorf("Unexpected matched path")
+	}
+}
+
+func TestMatcherFunc(t *testing.T) {
+	matcherFunc := func(*http.Request) bool {
+		return true
+	}
+
+	matcher := MatcherFunc(matcherFunc)
+
+	if !matcher.Match(&http.Request{}) {
+		t.Errorf("Unexpected not matched")
+	}
+}
+
+func TestMatcherFuncFail(t *testing.T) {
+	matcherFunc := func(*http.Request) bool {
+		return false
+	}
+
+	matcher := MatcherFunc(matcherFunc)
+
+	if matcher.Match(&http.Request{}) {
+		t.Errorf("Unexpected matched")
+	}
+}
